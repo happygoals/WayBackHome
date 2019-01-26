b957b2251ef8fc92742be9d038999d8dbb69b328
Haemin Ryu <happygoals@naver.com>
2019-01-26 14:13:08 -0500

    Third person controller style's movement(rotation)

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -14,7 +14,7 @@ public class CatController : MonoBehaviour
     private float moveZ;    // up, down
     private float speedH = 150f; // left, right
     private float speedZ = 150f;  // up, down
-
+    private float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,8 +40,49 @@ public class CatController : MonoBehaviour
         animator.SetFloat("v", v);
 
         moveX = h * speedH * Time.deltaTime; // check the direction key(left and right) , the distance of movement
-        moveZ = v * speedZ * Time.deltaTime; 
+        moveZ = v * speedZ * Time.deltaTime;
 
         rigidbody.velocity = new Vector3(moveX, 0, moveZ);
+
+        // Key movement : rotation + direction , Third Person Control Movement 
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (v >= 0)
+            {
+                this.transform.Rotate(0, -speed * 10 * Time.deltaTime, 0);
+            }
+            else
+            {
+                this.transform.Rotate(0, speed * 10 * Time.deltaTime, 0);
+            }
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (v >= 0)
+            {
+                this.transform.Rotate(0, speed * 10 * Time.deltaTime, 0);
+            }
+            else
+            {
+                this.transform.Rotate(0, -speed * 10 * Time.deltaTime, 0);
+            }
+        }
+    }
+
+
+    private void OnCoLlisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            Debug.Log("Collision Detected");
+        }
     }
 }
