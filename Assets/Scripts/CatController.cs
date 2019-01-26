786c7a79596c81f57cf0de20d3bc6359dac3a5b6
Haemin Ryu <happygoals@naver.com>
2019-01-26 10:29:06 -0500

    "Cat animation for 4 different direction(Up, Down, Left, Right) and Jump animation"

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class CatController : MonoBehaviour
 {
     public Animator animator;
+    public Rigidbody rigidbody;
 
     private float h;
     private float v;
 
-    private float moveX;
-    private float moveY;
-    private float speedH = 50f;
-    private float speedV = 80f;
+    private float moveX; // left, right
+    private float moveZ;    // up, down
+    private float speedH = 150f; // left, right
+    private float speedZ = 150f;  // up, down
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -27,7 +29,7 @@ public class CatController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            animator.transform.Translate(0, 0, speedH * Time.deltaTime);
             animator.Play("Run", -1, 0);
 
         }
@@ -37,5 +39,9 @@ public class CatController : MonoBehaviour
         animator.SetFloat("h", h);
         animator.SetFloat("v", v);
 
+        moveX = h * speedH * Time.deltaTime; // check the direction key(left and right) , the distance of movement
+        moveZ = v * speedZ * Time.deltaTime; 
+
+        rigidbody.velocity = new Vector3(moveX, 0, moveZ);
     }
 }
