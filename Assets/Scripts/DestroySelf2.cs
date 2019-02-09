4c24accfc27c57de10726f0b84c8ddb8d6216257
Haemin Ryu <happygoals@naver.com>
2019-02-09 12:32:27 -0500

    create main text

---
+++ Assets/Scripts/DestroySelf2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf2 : MonoBehaviour
+{
+    public float DestroyTime = 15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, DestroyTime);
+    }
+}
